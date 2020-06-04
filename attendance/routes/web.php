011eef44df2afcf388131d3c10505472d9bd1ca2
Ryo Yosida <ryo02110103@gmail.com>
2020-06-04 18:56:20 +0900

    ルーティング　フロントグループ化　リダイレクトグループ解除

--- attendance/routes/web.php
+++ attendance/routes/web.php
@@ -11,12 +11,17 @@
 |
 */
 
-Auth::routes();
+Route::redirect('/', 'home');
+Route::redirect('/admin', 'admin/home');
 
-//ログイン認証後
-Route::middleware('auth:user')->group(function() {
-    Route::redirect('/', 'home');
-    Route::get('home', 'HomeController@index')->name('home');
+//フロント
+Route::namespace('Front')->group(function() {
+    Auth::routes();
+    
+    //ログイン認証後
+    Route::middleware('auth:user')->group(function() {
+        Route::get('home', 'HomeController@index')->name('home');
+    });
 });
 
 //管理者
@@ -30,7 +35,6 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
 
     //ログイン認証後
     Route::middleware('auth:admin')->group(function() {
-        Route::redirect('/', 'admin/home');
-        Route::resource('home', 'HomeController', ['only' => 'index']);
+        Route::get('home', 'HomeController@index')->name('home');
     });
 });
