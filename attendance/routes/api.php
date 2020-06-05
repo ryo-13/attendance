f44c7bfa2050d5263a2df0b6262eca0e0ac44628
Ryo Yosida <ryo02110103@gmail.com>
2020-06-05 18:09:48 +0900

    UserControllerAPI ルーティング追加

--- attendance/routes/api.php
+++ attendance/routes/api.php
@@ -1,7 +1,4 @@
 <?php
-
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +10,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->namespace('Front\API')->group(function() {
+    Route::get('user', 'UserController@show');
+    Route::put('user', 'UserController@update');
 });
