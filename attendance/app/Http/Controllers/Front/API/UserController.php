ef283592db6f095a337a69825b773bb1f71a9fed
Ryo Yosida <ryo02110103@gmail.com>
2020-06-05 18:10:31 +0900

    UserControllerAPI作成

---
+++ attendance/app/Http/Controllers/Front/API/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Front\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\UpdateUser;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        return $request->user();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUser $request)
+    {
+        $request->user()->update($request->validated());
+    }
+}
