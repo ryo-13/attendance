d3e75cb0d316f6306996a8e0a80625cf77443ac4
Ryo Yosida <ryo02110103@gmail.com>
2020-06-05 15:50:40 +0900

    コントローラー不要なuse削除

--- attendance/app/Http/Controllers/Front/Auth/LoginController.php
+++ attendance/app/Http/Controllers/Front/Auth/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Front\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
