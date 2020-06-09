6b83fa30f85515a14c040b4de975f151aec0bb7f
Ryo Yosida <ryo02110103@gmail.com>
2020-06-09 18:02:26 +0900

    バリデーションエラー修正

--- attendance/app/Http/Requests/Front/API/UserUpdateRequest.php
+++ attendance/app/Http/Requests/Front/API/UserUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Front\API;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class UserUpdateRequest extends FormRequest
@@ -26,7 +27,7 @@ class UserUpdateRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->user)],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore(Auth::id())],
             'password' => 'nullable|alpha_dash|min:4|confirmed',
         ];
     }
