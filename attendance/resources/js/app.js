e44808663eff09b67d5f2b92ccf0d9ec835642e7
Ryo Yosida <ryo02110103@gmail.com>
2020-06-15 11:57:01 +0900

    dayjs設定

--- attendance/resources/js/app.js
+++ attendance/resources/js/app.js
@@ -2,22 +2,29 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import dayjs from 'dayjs'
+import 'dayjs/locale/ja';
+dayjs.locale('ja');
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+
 Vue.use(VueRouter)
+Vue.prototype.$dayjs = dayjs;
 
 Vue.component('navbar', require('./components/Layouts/Navbar.vue').default);
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/attendance', component: require('./components/Attendance.vue').default },
+        // { path: '/attendance', component: require('./components/Attendance.vue').default },
         { path: '/user', component: require('./components/User.vue').default },
     ]
 })
 
+
 new Vue({
     router,
-    el: '#app'
+    el: '#app',
 })
