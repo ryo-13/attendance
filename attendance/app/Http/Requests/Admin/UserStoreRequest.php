49b38cea08cd37f447767347fda794cc90075c4a
yamada-t <uto-k@MBP-0001.local>
2020-06-15 15:07:21 +0900

    管理者サイド 作業者一覧・作成・編集・削除の作成の修正

--- attendance/app/Http/Requests/Admin/UserStoreRequest.php
+++ attendance/app/Http/Requests/Admin/UserStoreRequest.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
-use App\Models\User;
 
 /**
  * Class StoreProductCategory
  * @package App\Http\Requests
  */
-class UpdateUser extends FormRequest
+class UserStoreRequest extends FormRequest
 {
 
     /**
@@ -28,13 +26,8 @@ class UpdateUser extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => [
-                'required',
-                'string',
-                'max:255',
-                Rule::unique(User::class)->ignore($this->user),
-            ],
-            'password' => 'nullable|alpha_dash|confirmed|min:4',
+            'email' => 'required|string|max:255|unique:App\Models\User',
+            'password' => 'required|alpha_dash|confirmed|min:4',
         ];
     }
 }
