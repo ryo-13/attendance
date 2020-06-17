6255bf21e75b311c257cedfb729c92561f388e5b
tsunemi-y <tatataabcd@gmail.com>
2020-06-17 12:02:46 +0900

    formRequestの名前変更

--- attendance/app/Http/Requests/Front/API/AttendanceUpdateRequest.php
+++ attendance/app/Http/Requests/Front/API/AttendanceUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Front\API;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreAttendanceRequest extends FormRequest
+class AttendanceUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -30,3 +30,5 @@ class StoreAttendanceRequest extends FormRequest
         ];
     }
 }
+
+
